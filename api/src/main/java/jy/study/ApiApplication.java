6072f82ec27532fd0f6147c615ad550b3b7d58e4
JY <kims8252@daum.net>
2022-07-18 00:38:58 +0900

    api 모듈 eureka client 설정 적용.

--- api/src/main/java/jy/study/ApiApplication.java
+++ api/src/main/java/jy/study/ApiApplication.java
@@ -2,7 +2,9 @@ package jy.study;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class ApiApplication {
 
